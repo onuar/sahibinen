ffd95de85352a964753ed3a7500fd01a5c36cbfd
onur.aykac <onur.aykac@trendyol.com>
2020-10-19 15:22:42 +0300

    Support hurriyetemlak

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 	chrome.extension.onRequest.addListener(function (request, sender, responseCallback) {
-		if (request.action === "getInfo") {
+		if (request.action !== "getInfo") {
+			alert("not implemented yet. maybe never.");
+		}
+		if (location.host === "www.sahibinden.com") {
 			let infos = $('.classifiedInfo li span');
 
 			let neighborhood = $('.classifiedInfo h2 a')[2].innerText;
@@ -28,8 +31,39 @@ $(document).ready(function () {
 				from: from,
 				link: link
 			});
-		} else {
-			alert("not implemented");
+		} else if (location.host === "www.hurriyetemlak.com") {
+			let neighborhood = $('.det-title-bottom .short-info-list li')[2].innerText + " Mh.";
+			let price = $('.det-title-upper .right p')[0].innerText;
+
+			let infos = $('.adv-info-list li');
+
+			let face = '';
+			$(infos[19]).find('div span').each(function () {
+				face += this.innerText;
+			});
+			face = face.replace(',', '').replace(' ', '');
+
+			let floor = infos[6].innerText.split('\n')[1].replace('. Kat', '') + " / " + infos[9].innerText.split('\n')[1].replace(' Katlı', '');
+			let room = infos[4].innerText.split('\n')[1];
+
+			let areaInfos = infos[5].innerText.split('\n')[1].split('/');
+			let area = areaInfos[1].replace(' m2', '') + " / " + areaInfos[0].replace(' m2', '');
+
+			let age = infos[7].innerText.split('\n')[1];
+			let from = "N/A";
+			let link = location.href;
+
+			responseCallback({
+				neighborhood: neighborhood,
+				price: price,
+				face: face,
+				floor: floor,
+				room: room,
+				area: area,
+				age: age,
+				from: from,
+				link: link
+			});
 		}
 	});
 });
